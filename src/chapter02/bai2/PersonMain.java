508f3d2a6d6706350258977d3aabff3d65dcc494
Nguyen Thi Huong Giang <morino091125@gmail.com>
2021-11-09 22:14:49 +0700

    done buoi 2

--- src/chapter02/bai2/PersonMain.java
+++ src/chapter02/bai2/PersonMain.java
@@ -3,7 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package chapter02;
+package chapter02.bai2;
+
+import chapter02.bai2.Person;
 
 /**
  *
